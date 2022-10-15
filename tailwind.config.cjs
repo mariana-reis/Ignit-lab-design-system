7960232554ebb39063a5d091bc986020e89bc8e0
mariana-reis <mariana.reis@al.resilia.com.br>
2022-10-15 19:29:04 -0300

    initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.tsx'],
+	theme: {
+		fontSize: {
+			xs: 14,
+			sm: 16,
+			md: 18,
+			lg: 20,
+			xl: 24,
+			'2xl': 32,
+		},
+		colors: {
+			transparent: 'transparent',
+
+			brack: '#000',
+			white: '#fff',
+
+			gray: {
+				900: '#121214',
+				800: '#202024',
+				400: '#7C7C8A',
+				100: '#E1E1E6',
+				200: '#C4C4CC',
+			},
+
+			cyan: {
+				500: '#81D8F7',
+				300: '#5FB9D9',
+			},
+		},
+		extend: {
+			fontFamily: {
+				sans: 'Inter, sans-serif',
+			},
+		},
+	},
+	plugins: [],
+};
