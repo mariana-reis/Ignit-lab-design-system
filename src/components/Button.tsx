a86ba23df54e0352ddc8c1987cfead14973a3157
mariana-reis <mariana.reis@al.resilia.com.br>
2022-10-17 12:58:07 -0300

    page creation

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,19 +1,20 @@
 import { clsx } from 'clsx';
 import { Slot } from '@radix-ui/react-slot';
 
-export interface ButtonProps {
+export interface ButtonProps extends ButtonHTMLAtributes<HTMLButtonElement> {
 	children: ReactNode;
 	asChild?: boolean;
 }
 
-export function Button({ children, asChild }: ButtonProps) {
+export function Button({ children, asChild, className, ...props }: ButtonProps) {
 	const Comp = asChild ? Slot : 'button';
 	return (
 		<Comp
 			className={clsx(
-				'py-4 px-3 bg-cyan-500 rounded font-semibold text-brack text-sm w-full transition-colors hover:bg-cyan-300 focus:ring-2 ring-gray-900',
-				{}
+				'py-3 px-4 bg-cyan-500 rounded font-semibold text-brack text-sm w-full transition-colors hover:bg-cyan-300 focus:ring-2 ring-gray-900',
+				className
 			)}
+			{...props}
 		>
 			{children}
 		</Comp>
